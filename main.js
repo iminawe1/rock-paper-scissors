253d90f1a7d7db4a839f7ed110b3c32e34e5393a
iminawe <sora.016@icloud.com>
2024-03-10 15:24:24 +0100

    create getHumanChoice function that takes a prompt input and prints it to the console and returns an error message if the input isnt rock, paper or scissors

--- main.js
+++ main.js
@@ -5,29 +5,26 @@
 //the final algorithm defines who is the winner or the loser
 //you know, ill get to this later, ill just go build sth physical for now, like a car, learning a new programming language is HARD
 
-function getComputerChoice() { 
-    let choices = ["rock","paper","scissors"];
-   let choiceInt =  Math.floor(Math.random() * choices.length); 
-   //choiceInt represents the position of the value in the array
-   let choice = choices[choiceInt]
-   //the choiceInt in the bracket calls for the value
-    return choice
+// function getComputerChoice() { 
+//     let choices = ["rock","paper","scissors"];
+//    let choiceInt =  Math.floor(Math.random() * choices.length); 
+//    //choiceInt represents the position of the value in the array
+//    let choice = choices[choiceInt]
+//    //the choiceInt in the bracket calls for the value
+//     return choice
+// }
+
+function getHumanChoice() { 
+    let input = prompt("1, 2, 3 go!")
+        inputLCase = input.toLowerCase()
+       if(inputLCase != "rock" && inputLCase != "scissors" && inputLCase != "paper") { 
+    return("please enter a valid option")
+    } else { 
+        return input
+    }
 }
 
-console.log(getComputerChoice())
-
-// function getHumanChoice() { 
-//     let input = prompt("1, 2, 3 go!")
-//         for(input == ) { 
-//             if(input == "rock") { 
-//                 alert("rock")
-//             } else if(input == "paper") { 
-//                 alert("paper!")
-//             } else if(input == "scissors") { 
-//                 alert("scissors!")
-//             }
-//         }
-// }
+console.log(getHumanChoice())
 
 //     function evaluateWinner(getComputerChoice, getHumanChoice) { 
 //         for()
